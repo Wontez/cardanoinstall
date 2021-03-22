05f93062019b0fc3ad34c5a37f89a47734f6a126
Wontez <51386078+Wontez@users.noreply.github.com>
2021-03-21 20:18:50 -0500

    Update debsetup.sh

--- debsetup.sh
+++ debsetup.sh
@@ -2,7 +2,7 @@
 
 # Run as the root user.
 apt install vim sudo &&
-update-alternatives --set editor /usr/bin/vim
+update-alternatives --set editor /usr/bin/vim.basic
 echo ""
 echo "Debian Setup Complete!"
 echo "Ready to add sudo users with visudo."
